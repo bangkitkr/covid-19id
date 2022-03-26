686d50e1a895fedc8ec8bb9151398d5942315d57
bangkitkr <102348274+bangkitkr@users.noreply.github.com>
2022-03-26 17:44:30 +0700

    Update dashboard_2.py

--- dashboard_2.py
+++ dashboard_2.py
@@ -1,5 +1,5 @@
 # Kita akan mulai dengan mengimpor beberapa library penting yang akan kita gunakan
-import matplotlib matplotlib.use('Agg')
+import matplotlib.use('Agg')
 from turtle import width
 import streamlit as st
 import pandas as pd
